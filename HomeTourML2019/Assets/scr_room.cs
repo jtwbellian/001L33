87100990dd0c780e0de61e65cacedcda65d46ecd
jtwbellian <32314548+jtwbellian@users.noreply.github.com>
2019-04-11 22:28:38 -0500

    mend

---
+++ HomeTourML2019/Assets/scr_room.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using UnityEngine.XR.MagicLeap;
+
+public class scr_room : MonoBehaviour
+{
+
+   public MLSpatialMapper sm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = MLSpatialMapper.instance;
+
+        foreach (GameObject o in sm.meshList)
+        {
+            o.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
