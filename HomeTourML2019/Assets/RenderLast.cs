edf3aaa57d15595ecd03dce936676d52bfb11a5f
jtwbellian <32314548+jtwbellian@users.noreply.github.com>
2019-04-15 15:08:58 -0500

    mend

--- HomeTourML2019/Assets/RenderLast.cs
+++ HomeTourML2019/Assets/RenderLast.cs
@@ -4,33 +4,45 @@ using UnityEngine;
 
 public class RenderLast : MonoBehaviour
 {
-
     private MeshRenderer[] renderers;
     private SkinnedMeshRenderer[] skinRenderers;
 
     public void SetOpaque(Material material)
     {
-        material.SetOverrideTag("RenderType", "");
-        material.SetOverrideTag("Queue", "Geometry");
-        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+        //material.SetOverrideTag("RenderType", "");
+        //material.SetOverrideTag("Queue", "Geometry");
+      // material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+       // material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
         material.SetInt("_ZWrite", 1);
-        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        material.renderQueue = 5000;
+        material.DisableKeyword("_ALPHAPREMULTIPLY_OFF");
+        material.renderQueue = 3000;
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
+        renderLast();
+
+        Debug.Log("Setting " + (renderers.Length + skinRenderers.Length) + "materials");
+    }
+
+    [ContextMenu("redraw")]
+    public void renderLast()
+    {
         renderers = GetComponentsInChildren<MeshRenderer>();
         skinRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
 
+        int i = 0;
+        int n = renderers.Length + skinRenderers.Length;
+
         foreach (MeshRenderer r in renderers)
         {
-            foreach(Material mat in r.materials )
+            foreach (Material mat in r.materials)
             {
                 SetOpaque(mat);
+                Debug.Log("updating renderer " + i + " / " + n);
+                i++;
             }
         }
 
@@ -39,15 +51,10 @@ public class RenderLast : MonoBehaviour
             foreach (Material mat in sr.materials)
             {
                 SetOpaque(mat);
+                Debug.Log("updating renderer " + i + " / " + n);
+                i++;
             }
         }
-
-        Debug.Log("Setting " + (renderers.Length + skinRenderers.Length) + "materials");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- HomeTourML2019/Assets/scr_room.cs
+++ HomeTourML2019/Assets/scr_room.cs
@@ -7,23 +7,65 @@ using UnityEngine.XR.MagicLeap;
 
 public class scr_room : MonoBehaviour
 {
+    public bool seen = false;
+    public MLSpatialMapper sm;
+    private Animator anim;
 
-   public MLSpatialMapper sm;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        /*anim = GetComponent<Animator>();
+        anim.SetBool("shut", true);
+
         sm = MLSpatialMapper.instance;
 
         foreach (GameObject o in sm.meshList)
         {
-            o.SetActive(false);
+            MeshRenderer mr = o.GetComponent<MeshRenderer>();
+            mr.enabled = false;
+        }*/
+    }
+   
+    public void Update()
+    {
+
+    }
+
+    public void OnSeen()
+    {
+        anim.SetBool("shut", false);
+        HideRealWorld();
+        seen = true;
+    }
+
+
+    public void OnHidden()
+    {
+        anim.SetBool("shut", true);
+        ShowRealWorld();
+        seen = false;
+    }
+
+    public void HideRealWorld()
+    {
+
+        Debug.Log("MeshRenderer Hidden");
+        foreach (GameObject o in sm.meshList)
+        {
+            MeshRenderer mr = o.GetComponent<MeshRenderer>();
+            mr.enabled = false;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowRealWorld()
     {
-        
+        Debug.Log("MeshRenderer Show");
+        foreach (GameObject o in sm.meshList)
+        {
+            MeshRenderer mr = o.GetComponent<MeshRenderer>();
+            mr.enabled = true;
+        }
     }
 }
