17fb1cbf0cb6bd4631e3f6b0c5d2249001dfa385
jtwbellian <32314548+jtwbellian@users.noreply.github.com>
2019-04-14 22:33:45 -0500

    mend

---
+++ HomeTourML2019/Assets/scr_anim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_anim : MonoBehaviour
+{
+    [SerializeField]
+    private string stateName = "NONE";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator anim = GetComponent<Animator>();
+        anim.Play(stateName, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
